555607c0e87b9e7ff0082492789f64dc0c4bb372
Edin Jašarević <edinjasar14@gmail.com>
2021-07-27 02:00:20 +0200

    init

---
+++ ShoppingCart/Program.cs
@@ -0,0 +1,40 @@
+﻿using ShoppingCart.Business.Repositories;
+using System;
+
+namespace ShoppingCart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var shoppingCartRepository = new ShoppingCartRepository();
+            var productsRepository = new ProductsRepository();
+
+            var product = productsRepository.FindBy("SM7B");
+
+            shoppingCartRepository.Add(product);
+            shoppingCartRepository.IncreaseQuantity(product.ArticleId);
+            shoppingCartRepository.IncreaseQuantity(product.ArticleId);
+            shoppingCartRepository.IncreaseQuantity(product.ArticleId);
+            shoppingCartRepository.IncreaseQuantity(product.ArticleId);
+            
+            PrintCart(shoppingCartRepository);
+        }
+
+        static void PrintCart(ShoppingCartRepository shoppingCartRepository)
+        {
+            var totalPrice = 0m;
+            foreach (var lineItem in shoppingCartRepository.LineItems)
+            {
+                var price = lineItem.Value.Product.Price * lineItem.Value.Quantity;
+
+                Console.WriteLine($"{lineItem.Key} " +
+                    $"${lineItem.Value.Product.Price} x {lineItem.Value.Quantity} = ${price}");
+
+                totalPrice += price;
+            }
+
+            Console.WriteLine($"Total price:\t${totalPrice}");
+        }
+    }
+}
