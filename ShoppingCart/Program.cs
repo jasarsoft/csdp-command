3e77c60ef02663b361feb5d53b87b052b2df1f6a
Edin Jašarević <edinjasar14@gmail.com>
2021-07-27 21:14:18 +0200

    the command pattern in action

--- ShoppingCart/Program.cs
+++ ShoppingCart/Program.cs
@@ -1,4 +1,5 @@
-﻿using ShoppingCart.Business.Repositories;
+﻿using ShoppingCart.Business.Commands;
+using ShoppingCart.Business.Repositories;
 using System;
 
 namespace ShoppingCart
@@ -12,12 +13,27 @@ namespace ShoppingCart
 
             var product = productsRepository.FindBy("SM7B");
 
-            shoppingCartRepository.Add(product);
-            shoppingCartRepository.IncreaseQuantity(product.ArticleId);
-            shoppingCartRepository.IncreaseQuantity(product.ArticleId);
-            shoppingCartRepository.IncreaseQuantity(product.ArticleId);
-            shoppingCartRepository.IncreaseQuantity(product.ArticleId);
+            var addToCartCommand = new AddToCartCommand(shoppingCartRepository,
+                productsRepository,
+                product);
+
+            var increaseQuantityCommand = new ChangeQuantityCommand(
+                ChangeQuantityCommand.Operation.Increase,
+                shoppingCartRepository,
+                productsRepository,
+                product);
+
+            var manager = new CommandManager();
+            manager.Invoke(addToCartCommand);
+            manager.Invoke(increaseQuantityCommand);
+            manager.Invoke(increaseQuantityCommand);
+            manager.Invoke(increaseQuantityCommand);
+            manager.Invoke(increaseQuantityCommand);
             
+            PrintCart(shoppingCartRepository);
+
+            manager.Undo();
+
             PrintCart(shoppingCartRepository);
         }
 
